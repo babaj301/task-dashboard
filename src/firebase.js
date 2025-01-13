079d85eff06a8c0a00a58390f316a32d4a09b0b8
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-01-13 12:24:30 +0100

    Added env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase Config Object
 const firebaseConfig = {
-  apiKey: 'AIzaSyDCqoevjhtj3wHQSzsPQzcXyfSCZLz3VkI',
-  authDomain: 'todo-project-301.firebaseapp.com',
-  projectId: 'todo-project-301',
-  storageBucket: 'todo-project-301.firebasestorage.app',
-  messagingSenderId: '400290365752',
-  appId: '1:400290365752:web:de9ecfaf365b4973c3c1cd',
-  measurementId: 'G-5F56KL3GM8',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
